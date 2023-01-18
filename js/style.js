934ddba303ac72871078dbe5b1d23e3b3d762dd7
jsk_pub <jsk.pubwork@gmail.com>
2023-01-18 10:54:40 +0900

    pf source 수정

--- js/style.js
+++ js/style.js
@@ -9,7 +9,7 @@ function topMenuBtn() {
 // gnb 메뉴 선택시 스크롤 이동
 function scrollGnb() {
   $('#header h1, .gnb li').click(function () {
-    var scrollPosition = $($(this).find('a').attr('href')).offset().top;
+    let scrollPosition = $($(this).find('a').attr('href')).offset().top;
 
     $('html, body').animate(
       {
@@ -20,7 +20,7 @@ function scrollGnb() {
   });
 }
 
-var aboutAnimation = function () {
+let aboutAnimation = function () {
   $(window).scroll(function () {
     $('.about_con > dl').addClass('on');
   });
@@ -31,7 +31,7 @@ clearTimeout(aboutAnimation, 1000);
 // skill영역 스크롤 이동 시 애니메이션 제어
 function skillAnimation() {
   $(window).scroll(function () {
-    var currentPostion = $('html, body').scrollTop();
+    let currentPostion = $('html, body').scrollTop();
 
     skillList = $('.progress_list li');
 
@@ -66,80 +66,62 @@ function skillAnimation() {
 //   }
 // }
 
-// var x = window.matchMedia('(min-width:799px)');
+// let x = window.matchMedia('(min-width:799px)');
 // mediaFunc(x);
 
 /* portfolio  popup*/
-
-function portfolioPopup() {
-  $('#thinktree').animatedModal({ modalTarget: 'ttModal' });
-  $('#mot').animatedModal({ modalTarget: 'motModal' });
-  $('#rollpop').animatedModal({ modalTarget: 'rollpopModal' });
-  $('#fourdshop').animatedModal({ modalTarget: 'fourdshopModal' });
-  $('#hanam').animatedModal({ modalTarget: 'hanamModal' });
-  $('#item').animatedModal({ modalTarget: 'itemModal' });
-  $('#vap').animatedModal({ modalTarget: 'vapModal' });
-  $('#dietefit').animatedModal({ modalTarget: 'dietefitModal' });
-  $('#iwedding').animatedModal({ modalTarget: 'iweddingModal' });
-  $('#iweddingb').animatedModal({ modalTarget: 'iweddingbModal' });
-  $('#icolor').animatedModal({ modalTarget: 'icolorModal' });
-  $('#sportsguru').animatedModal({ modalTarget: 'sportsguruModal' });
-  $('#hyundaicapital').animatedModal({ modalTarget: 'hyundaicapitalModal' });
-  $('#busandbt').animatedModal({ modalTarget: 'busandbtModal' });
-  $('#hyundaicard').animatedModal({ modalTarget: 'hyundaicardModal' });
-  $('#lguplus').animatedModal({ modalTarget: 'lguplusModal' });
-  $('#kmiAdmin').animatedModal({ modalTarget: 'kmiAdminModal' });
-  $('#kmiPc').animatedModal({ modalTarget: 'kmiPcModal' });
-  $('#kmiMobile').animatedModal({ modalTarget: 'kmiMobileModal' });
-}
-
 /* poftfolio img */
 function poftfolioImg() {
   $(window).resize(function () {
-    var imgHeight = $('.pop_img li').height();
+    let imgHeight = $('.pop_img li').height();
     $('.pop_img ul').height(imgHeight);
-  });
-  $('.more_btn').click(function () {
-    var imgHeight = $('.pop_img li').height();
-    $('.pop_img ul').height(imgHeight);
-    var select = $(this).attr('href');
-    var $imgList = $(select).find('.pop_img li');
-    var nImgCount = $imgList.children().length;
-    var nIndex = 0;
-
-    // reset
-    $($imgList.get(0)).css('display', 'block');
-    $($imgList.get(1), $imgList.get(2)).css('display', 'none');
-    $($imgList.get(2)).css('display', 'none');
-
-    var repeat = setInterval(autoSlide, 2000);
-
-    function autoSlide() {
-      var next = ++nIndex % nImgCount;
-      $($imgList.get(next - 1)).fadeOut(1000);
-      $($imgList.get(next)).fadeIn(1000);
-    }
-
-    // stop image
-    $('.btn-close-modal').click(function () {
-      clearInterval(repeat);
-    });
   });
 }
 
+$('.pf_img, .more_btn').click(function () {
+  let pfTarget = $(this).attr('id');
+  $(`#${pfTarget}`).animatedModal({ modalTarget: `${pfTarget}Modal` });
+  let imgHeight = $('.pop_img li').height();
+  $('.pop_img ul').height(imgHeight);
+  let select = $(this).attr('href');
+  let $imgList = $(select).find('.pop_img li');
+  let nImgCount = $imgList.children().length;
+  let nIndex = 0;
+
+  // reset
+  $($imgList.get(0)).css('display', 'block');
+  $($imgList.get(1), $imgList.get(2)).css('display', 'none');
+  $($imgList.get(2)).css('display', 'none');
+
+  let repeat = setInterval(autoSlide, 2000);
+
+  function autoSlide() {
+    let next = ++nIndex % nImgCount;
+    $($imgList.get(next - 1)).fadeOut(1000);
+    $($imgList.get(next)).fadeIn(1000);
+  }
+
+  // stop image
+  $('.btn-close-modal').click(function () {
+    window.location.hash = '';
+    window.location.href.replace('#', '');
+    console.log(window.location.href);
+
+    clearInterval(repeat);
+  });
+});
+
 $(function () {
-  //videoHeight();
   topMenuBtn();
   scrollGnb();
   skillAnimation();
-  portfolioPopup();
   poftfolioImg();
 });
 
 // 스크롤이 발생하면 메뉴 보이기
 function scrollHeader() {
   $(window).on('scroll', function () {
-    var scrollPosition = $(this).scrollTop();
+    let scrollPosition = $(this).scrollTop();
 
     if (scrollPosition > 200) {
       $('#header').addClass('on');
@@ -151,4 +133,23 @@ function scrollHeader() {
   });
 }
 scrollHeader();
-clearTimeout(scrollHeader, 1000);
+//clearTimeout(scrollHeader, 1000);
+
+// $('#thinktree').animatedModal({ modalTarget: 'ttModal' });
+// $('#mot').animatedModal({ modalTarget: 'motModal' });
+// $('#rollpop').animatedModal({ modalTarget: 'rollpopModal' });
+// $('#fourdshop').animatedModal({ modalTarget: 'fourdshopModal' });
+// $('#hanam').animatedModal({ modalTarget: 'hanamModal' });
+// $('#item').animatedModal({ modalTarget: 'itemModal' });
+// $('#vap').animatedModal({ modalTarget: 'vapModal' });
+// $('#dietefit').animatedModal({ modalTarget: 'dietefitModal' });
+// $('#iwedding').animatedModal({ modalTarget: 'iweddingModal' });
+// $('#iweddingb').animatedModal({ modalTarget: 'iweddingbModal' });
+// $('#icolor').animatedModal({ modalTarget: 'icolorModal' });
+// $('#sportsguru').animatedModal({ modalTarget: 'sportsguruModal' });
+// $('#hyundaicapital').animatedModal({ modalTarget: 'hyundaicapitalModal' });
+// $('#busandbt').animatedModal({ modalTarget: 'busandbtModal' });
+// $('#hyundaicard').animatedModal({ modalTarget: 'hyundaicardModal' });
+// $('#lguplus').animatedModal({ modalTarget: 'lguplusModal' });
+// $('#kmiAdmin').animatedModal({ modalTarget: 'kmiAdminModal' });
+// $('#kmiPc').animatedModal({ modalTarget: 'kmiPcModal' });
